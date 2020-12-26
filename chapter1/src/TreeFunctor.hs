9a4b92137d37000940f37862da7bd7da06db7a99
Yoshiya Noda <ny1425.0106@gmail.com>
2020-12-26 22:54:18 +0900

    Tree関手書き換え

--- chapter1/src/TreeFunctor.hs
+++ chapter1/src/TreeFunctor.hs
@@ -2,7 +2,9 @@ module MyTreeFunctor where
 import Data.Char
 
 ----これは型構築子Treeを宣言している
-
+----構文規則のBN記法のようなもの
 data Tree a = Empty | Node a (Tree a) (Tree a)
 
-
+--instanceとは
+instance (Show a) => Show (Tree a) where
+    show x = show1 0 x
